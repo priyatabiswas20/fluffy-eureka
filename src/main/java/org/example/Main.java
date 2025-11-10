3855be7461bad771b912b7a377ec6c3de2a6349b
Priyata Biswas <priyata.biswas20@gmail.com>
2025-11-10 20:33:23 +0530

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+        /*IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }*/
+    }
+}
